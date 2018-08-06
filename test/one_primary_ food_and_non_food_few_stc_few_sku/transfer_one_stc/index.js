5ffa9b541315cea12018108b988053aec95d8c14
codemagnus <brenthmiras2@gmail.com>
2018-08-06 18:20:45 +0800

    Add reset data

--- test/one_primary_ food_and_non_food_few_stc_few_sku/transfer_one_stc/index.js
+++ test/one_primary_ food_and_non_food_few_stc_few_sku/transfer_one_stc/index.js
@@ -6,72 +6,82 @@ const chai = require('chai');
 
 let token;
 
-describe('POST /auth/login', function () {
-    it('should be able to login', function (done) {
+before('Login user to get token', function (done) {
+    this.timeout(20000);
+    request
+    .post('/auth/login')
+    .type('json')
+    .set('Content-Type', 'application/json')
+    .send({
+        email: 'admin@cdi.com',
+        password: 'secret'
+    })
+    .end(function(err, result){
+        token = result.header['x-access-token'];
+        done();
+    });
+});
+
+describe('POST /test/reset', function () {
+    it('should be able to reset data', function (done) {
         // Pass
         this.timeout(20000);
         request
-            .post('/auth/login')
-            .type('json')
-            .set('Content-Type', 'application/json')
-            .send({
-                "email": "admin@cdi.com",
-                "password": "secret"
-            })
-            .expect(200, function (err, result) {
+        .post('/test/reset')
+        .type('json')
+        .set('Content-Type', 'application/json')
+        .set('x-access-token', token)
+        .send()
+        .expect(200, function (err, result) {
 
-                chai.expect(result.header).to.have.property('x-access-token');
-
-                token = result.header['x-access-token'];
-
-                describe('Order management', function () {
-                    require('./order_management')(token, request);
-                });
-
-                describe('Resource plan', function () {
-                    require('./resource_plan')(token, request);
-                });
-
-                describe('Inbound', function () {
-                    require('./item_movement/inbound')(token, request);
-                });
-
-                describe('Putaway', function () {
-                    require('./item_movement/putaway')(token, request);
-                });
-
-                describe('Primary shipment plan', function () {
-                    require('./primary_shipment_plan')(token, request);
-                });
-
-                describe('Secondary plan', function () {
-                    require('./secondary_shipment_plan')(token, request);
-                });
-
-                describe('Inbound', function () {
-                    require('./item_movement/inbound')(token, request);
-                });
-                describe('Sorting', function () {
-                    require('./item_movement/sorting')(token, request);
-                });
-
-                describe('Outbound', function () {
-                    require('./item_movement/outbound')(token, request);
-                });
-
-                describe('Loading', function () {
-                    require('./item_movement/loading')(token, request);
-                });
-
-                describe('Reports', function () {
-                    require('./reports')(token, request);
-                });
-
-                describe('Dashboard', function () {
-                    require('./dashboard')(token, request);
-                });
-
-                done();
+            describe('Order management', function () {
+                require('./order_management')(token, request);
             });
+
+            describe('Resource plan', function () {
+                require('./resource_plan')(token, request);
+            });
+
+            describe('Inbound', function () {
+                require('./item_movement/inbound')(token, request);
+            });
+
+            describe('Putaway', function () {
+                require('./item_movement/putaway')(token, request);
+            });
+
+            describe('Primary shipment plan', function () {
+                require('./primary_shipment_plan')(token, request);
+            });
+
+            describe('Secondary plan', function () {
+                require('./secondary_shipment_plan')(token, request);
+            });
+
+            describe('Inbound', function () {
+                require('./item_movement/inbound')(token, request);
+            });
+            describe('Sorting', function () {
+                require('./item_movement/sorting')(token, request);
+            });
+
+            describe('Outbound', function () {
+                require('./item_movement/outbound')(token, request);
+            });
+
+            describe('Loading', function () {
+                require('./item_movement/loading')(token, request);
+            });
+
+            describe('Reports', function () {
+                require('./reports')(token, request);
+            });
+
+            describe('Dashboard', function () {
+                require('./dashboard')(token, request);
+            });
+
+            done();
+        });
     });
 });
