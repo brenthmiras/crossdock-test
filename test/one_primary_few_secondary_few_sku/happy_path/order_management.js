d8e9357429d498825a0ac2c5462ca168c33f1b9b
christan <christan@codedisruptors.com>
2018-08-06 08:47:58 +0800

    add package.json, add login

--- test/one_primary_few_secondary_few_sku/happy_path/order_management.js
+++ test/one_primary_few_secondary_few_sku/happy_path/order_management.js
@@ -1,3 +1,27 @@
+const request = require('supertest');
+
+let token;
+
+describe('POST /auth/login', function () {
+    it('should be able to login', function () {
+        // Pass
+        request('13.250.63.204:8001')
+            .post('/auth/login')
+            .type('json')
+            .set('Content-Type', 'application/json')
+            .send({
+                "email": "admin@cdi.com",
+                "password": "secret"
+            }).end(function (errr, result) {
+
+                token = result.header['x-access-token'];
+
+            });
+    });
+    chai.expect({}).to.not.have.property("token");
+});
+
+
 describe('POST /prealert', function () {
     it('should be successful', function () {
         // Pass
