07f74f427e01f0ea695b23ce9f63ed6bbf55767b
christan <christan@codedisruptors.com>
2018-08-06 09:12:47 +0800

    add test for login, add gitignore

--- test/one_primary_few_secondary_few_sku/happy_path/order_management.js
+++ test/one_primary_few_secondary_few_sku/happy_path/order_management.js
@@ -1,24 +1,31 @@
 const request = require('supertest');
+const chai = require('chai');
 
 let token;
 
 describe('POST /auth/login', function () {
-    it('should be able to login', function () {
+    it('should be able to login', function (done) {
         // Pass
+        
         request('13.250.63.204:8001')
             .post('/auth/login')
             .type('json')
+            .timeout(10000)
             .set('Content-Type', 'application/json')
             .send({
                 "email": "admin@cdi.com",
                 "password": "secret"
-            }).end(function (errr, result) {
+            })
+            .expect(200, function (err, result) {
+
+                chai.expect(result.header).to.have.property('x-access-token');
 
                 token = result.header['x-access-token'];
+                console.log(token);
+                done();
+            })
 
-            });
     });
-    chai.expect({}).to.not.have.property("token");
 });
 
 
