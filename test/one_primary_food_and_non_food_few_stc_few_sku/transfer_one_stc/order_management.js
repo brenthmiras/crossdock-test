7ff1dd6f7d0d95082cfe55c6475ffbdf5fa7dcc1
codemagnus <brenthmiras2@gmail.com>
2018-08-07 09:42:51 +0800

    GET /bookings?status=prealert - Add test

--- test/one_primary_food_and_non_food_few_stc_few_sku/transfer_one_stc/order_management.js
+++ test/one_primary_food_and_non_food_few_stc_few_sku/transfer_one_stc/order_management.js
@@ -2,10 +2,12 @@ const chai = require('chai');
 const expect = chai.expect;
 
 module.exports = function(token, request) {
-
+    // Use our prealert with:
+    // 24 rows
     const filePath = '../prealert.csv';
 
     describe('POST /prealerts', function () {
+        this.timeout(5000);
         it('should be successful', function (done) {
             request
             .post('/prealerts')
@@ -21,8 +23,27 @@ module.exports = function(token, request) {
     });
 
     describe('GET /bookings?status=prealert', function () {
+        let result;
+
+        before('Fire http request', function (done) {
+            request
+            .get('/bookings?status=prealert')
+            .set('Content-Type', 'multipart/form-data')
+            .set('x-access-token', token)
+            .send()
+            .end(function(err, res) {
+                result = res;
+                done();
+            });
+        });
+        
         it('should be successful', function () {
-            // Pass
+            expect(result.status).to.equal(200);
+        });
+
+        it('should contain the prealert we uploaded', function () {
+            const rows = result.body.data.items;
+            expect(rows.length).to.equal(1);
         });
     });
 }
