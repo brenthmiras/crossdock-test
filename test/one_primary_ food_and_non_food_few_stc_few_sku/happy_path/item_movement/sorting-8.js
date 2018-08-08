bf5a7db0e624cc205b4ee935762d27002e999988
Guest <alinsod3jay@gmail.com>
2018-08-08 18:09:56 +0800

    add test cases for inbound, putaway and sorting

--- test/one_primary_ food_and_non_food_few_stc_few_sku/happy_path/item_movement/sorting-8.js
+++ test/one_primary_ food_and_non_food_few_stc_few_sku/happy_path/item_movement/sorting-8.js
@@ -14,7 +14,7 @@ module.exports = function (token, request) {
                     "source_container": "ZEU800000001",
                     "destination_container": "GRC-005",
                     "quantity": 8,
-                    "sku": "67106205",
+                    "sku": "615999",
                     "ship_to_code": "STC-000000531"
                 })
                 .expect(200, function (err, result) {
@@ -25,16 +25,16 @@ module.exports = function (token, request) {
         });
 
 
-        it('it should sort ZEU800000002 successfully', function (done) {
+        it('it should sort ZEU800000001 successfully', function (done) {
 
             request
                 .post('/item/sort')
                 .set('x-access-token', token)
                 .send({
-                    "source_container": "ZEU800000002",
+                    "source_container": "ZEU800000001",
                     "destination_container": "GRC-004",
                     "quantity": 8,
-                    "sku": "67106205",
+                    "sku": "615999",
                     "ship_to_code": "STC-000000534"
                 })
                 .expect(200, function (err, result) {
@@ -44,17 +44,16 @@ module.exports = function (token, request) {
 
         });
 
-
-        it('it should sort ZEU800000003 successfully', function (done) {
+        it('it should sort ZEU800000001 successfully', function (done) {
 
             request
                 .post('/item/sort')
                 .set('x-access-token', token)
                 .send({
-                    "source_container": "ZEU800000003",
+                    "source_container": "ZEU800000001",
                     "destination_container": "GRC-002",
                     "quantity": 8,
-                    "sku": "67106205",
+                    "sku": "615999",
                     "ship_to_code": "STC-000000616"
                 })
                 .expect(200, function (err, result) {
