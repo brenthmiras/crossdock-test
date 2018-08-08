7099edc23d63978302133b9eac7b0719c8b311ec
tanjarce <tanjarce@yahoo.com.ph>
2018-08-08 19:26:33 +0800

    add tests for damaged, excess and short

---
+++ test/one_primary_ food_and_non_food_few_stc_few_sku/damaged_excess_short/item_movement/inbound-05.js
@@ -0,0 +1,45 @@
+const chai = require('chai');
+
+module.exports = function (token, request) {
+
+    describe('GET /inbound', function () {
+
+        it('it should receive ZEU500000001 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180808-001',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU500000001',
+                    'material_id': '688368b7-adf3-4712-ae8d-2bca4e05eea8',
+                    'quantity': 20
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+        it('it should receive ZEU500000002 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180808-001-EXCESS',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU500000002',
+                    'material_id': '688368b7-adf3-4712-ae8d-2bca4e05eea8',
+                    'quantity': 3
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+
+    });
+};
