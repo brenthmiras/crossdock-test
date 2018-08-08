ed19fdf882ace3b0db700f60191ceed11557b8d6
danlenard <danhacutina@gmail.com>
2018-08-08 18:04:04 +0800

    add test cases for inbound, putaway and sorting

--- test/one_primary_ food_and_non_food_few_stc_few_sku/happy_path/index.js
+++ test/one_primary_ food_and_non_food_few_stc_few_sku/happy_path/index.js
@@ -6,78 +6,91 @@ const chai = require('chai');
 
 let token;
 
-before('Login user to get token', function (done) {
-    this.timeout(20000);
-    request
-    .post('/auth/login')
-    .type('json')
-    .set('Content-Type', 'application/json')
-    .send({
-        email: 'admin@cdi.com',
-        password: 'secret'
-    })
-    .end(function(err, result){
-        token = result.header['x-access-token'];
-        done();
-    });
-});
-
-describe('POST /test/reset', function () {
-    it('should be able to reset data', function (done) {
-        // Pass
+describe('Login user to get token', function () {
+    it('should be successful', function(done){
         this.timeout(20000);
         request
-        .post('/test/reset')
+        .post('/auth/login')
         .type('json')
         .set('Content-Type', 'application/json')
-        .set('x-access-token', token)
-        .send()
-        .expect(200, function (err, result) {
-
-            describe('Order management', function () {
-                require('./order_management')(token, request);
-            });
-
-            describe('Primary shipment plan', function () {
-                require('./primary_shipment_plan')(token, request);
-            });
-
-            describe('Secondary plan', function () {
-                require('./secondary_shipment_plan')(token, request);
-            });
-
-            describe('Resource plan', function () {
-                require('./resource_plan')(token, request);
-            });
-
+        .send({
+            email: 'admin@cdi.com',
+            password: 'secret'
+        })
+        .end(function(err, result){
+            token = result.header['x-access-token'];
             describe('Inbound', function () {
-                require('./item_movement/inbound')(token, request);
+                require('./item_movement/inbound-01')(token, request);
             });
-
+    
             describe('Putaway', function () {
-                require('./item_movement/putaway')(token, request);
+                require('./item_movement/putaway-01')(token, request);
             });
+
             describe('Sorting', function () {
-                require('./item_movement/sorting')(token, request);
+                require('./item_movement/sorting-01')(token, request);
             });
-
-            describe('Outbound', function () {
-                require('./item_movement/outbound')(token, request);
-            });
-
-            describe('Loading', function () {
-                require('./item_movement/loading')(token, request);
-            });
-
-            describe('Reports', function () {
-                require('./reports')(token, request);
-            });
-
-            describe('Dashboard', function () {
-                require('./dashboard')(token, request);
-            });
-
             done();
         });
-    });
+    })
 });
+
+// describe('POST /test/reset', function () {
+//     // it('should be able to reset data', function (done) {
+//     //     // Pass
+//     //     this.timeout(20000);
+//     //     request
+//     //     .post('/test/reset')
+//     //     .type('json')
+//     //     .set('Content-Type', 'application/json')
+//     //     .set('x-access-token', token)
+//     //     .send()
+//     //     .expect(200, function (err, result) {
+
+//     //         describe('Order management', function () {
+//     //             require('./order_management')(token, request);
+//     //         });
+
+//     //         describe('Primary shipment plan', function () {
+//     //             require('./primary_shipment_plan')(token, request);
+//     //         });
+
+//     //         describe('Secondary plan', function () {
+//     //             require('./secondary_shipment_plan')(token, request);
+//     //         });
+
+//     //         describe('Resource plan', function () {
+//     //             require('./resource_plan')(token, request);
+//     //         });
+
+//             describe('Inbound', function () {
+//                 require('./item_movement/inbound')(token, request);
+//             });
+
+//             describe('Putaway', function () {
+//                 require('./item_movement/putaway')(token, request);
+//             });
+//             // describe('Sorting', function () {
+//             //     require('./item_movement/sorting')(token, request);
+//             // });
+
+//             // describe('Outbound', function () {
+//             //     require('./item_movement/outbound')(token, request);
+//             // });
+
+//             // describe('Loading', function () {
+//             //     require('./item_movement/loading')(token, request);
+//             // });
+
+//             // describe('Reports', function () {
+//             //     require('./reports')(token, request);
+//             // });
+
+//             // describe('Dashboard', function () {
+//             //     require('./dashboard')(token, request);
+//             // });
+
+//             done();
+//         // });
+//     // });
+// });
