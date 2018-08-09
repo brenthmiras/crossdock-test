79747f3421b16d6ebae5aa4798ce388b51a38b4e
skysemilla <fpsemilla@up.edu.ph>
2018-08-09 18:05:07 +0800

    add test for happy path test file 3

--- test/test-file-3/happy_path/item_movement/inbound-07.js
+++ test/test-file-3/happy_path/item_movement/inbound-07.js
@@ -1,9 +1,51 @@
 const chai = require('chai');
-
+//
 module.exports = function (token, request) {
-//20046526
+
     describe('GET /inbound', function () {
 
+        //08-08-1
+        //616704
+        it('it should receive ZEU700000001 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180809-002',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU700000001',
+                    'material_id': '8aa7e29d-6980-4e51-bc68-22b08b72865c',
+                    'quantity': 40
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+
+        it('it should receive ZEU700000002 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180809-002',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU700000002',
+                    'material_id': '8aa7e29d-6980-4e51-bc68-22b08b72865c',
+                    'quantity': 40
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+
+        //08-09-1
+        //20046526
         it('it should receive ZEU770000001 successfully', function (done) {
             // Pass
             request
@@ -11,7 +53,7 @@ module.exports = function (token, request) {
                 .set('x-access-token', token)
                 .type('json')
                 .send({
-                    'source_container_location': 'ID-180809-001',
+                    'source_container_location': 'ID-180809-003',
                     'source_container': 'MAX999',
                     'destination_container': 'ZEU770000001',
                     'material_id': 'd1650e4a-058d-4896-9794-337cd4ab05a1',
@@ -23,6 +65,25 @@ module.exports = function (token, request) {
                 });
         });
         
-
+        //08-09-2
+        //JF000913
+        it('it should receive ZEU777000001 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180809-001',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU777000001',
+                    'material_id': 'b404acdd-a0b8-4adc-80c4-5f6dccf53573',
+                    'quantity': 9
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
     });
 };
--- test/test-file-3/happy_path/item_movement/putaway-07.js
+++ test/test-file-3/happy_path/item_movement/putaway-07.js
@@ -3,6 +3,37 @@ const chai = require('chai');
 module.exports = function (token, request) {
 
     describe('POST /item/putaway', function () {
+
+        //08-08-1
+        //616704
+        it('it should putaway ZEU700000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU700000001',
+                    'destination_container_location': 'GSTG-A01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should putaway ZEU700000002 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU700000002',
+                    'destination_container_location': 'GSTG-A01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        //08-09-1
+        //20046526
         it('it should putaway ZEU770000001 successfully', function (done) {
             request
                 .post('/item/putaway')
@@ -16,5 +47,20 @@ module.exports = function (token, request) {
                 });
         });
 
+        //08-09-2
+        //JF000913
+        it('it should putaway ZEU777000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU777000001',
+                    'destination_container_location': 'GSTG-B01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+        
     });
 };
