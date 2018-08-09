92f9daace86053e0fe5a5f17b4c2b48bf2ce6652
christan <christan@codedisruptors.com>
2018-08-09 14:26:35 +0800

    add inbound, putaway and sort tests

---
+++ test/test-file-2/damaged_excess_short/order_management.js
@@ -0,0 +1,30 @@
+
+module.exports = function(token, request) {
+
+
+    describe('POST /prealert', function () {
+        it('File upload in prealert', function () { 
+            // request
+            //     .post('/prealerts')
+            //     .attach('file', './prealert.csv', 'prealert.csv')
+            //     .set('Content-Type', 'text/csv')
+            //     .set({'Content-Disposition': 'form-data', 'name': 'file', 'filename': 'prealert.csv'})
+            //     .set('x-access-token', token)
+            //     .expect(200)
+            //     .end((err, res) => {
+            //         if(err) {
+            //             console.log(err);
+            //         }
+            //         console.log(res.body.data);
+            //         done();
+            //     });
+        });
+    });
+
+    // describe('GET /bookings?status=prealert', function () {
+    //     it('should be successful', function () {
+    //         // Pass
+    //     });
+    // });
+}
+
