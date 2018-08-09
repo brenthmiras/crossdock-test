01a0b385e90a422b0c4cb9ac80c883e7ec6f77fb
stacieH <stacie.harlene@gmail.com>
2018-08-09 06:14:19 -0500

    add test cases for inbound, putaway and sorting

---
+++ test/one_primary_ food_and_non_food_few_stc_few_sku/happy_path/item_movement/inbound-3.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+
+module.exports = function (token, request) {
+
+    describe('GET /inbound', function () {
+
+        it('it should receive ZEU300000001 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180808-001',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU300000001',
+                    'material_id': '18568205-3205-4c02-b0fb-0c204dd7cc46',
+                    'quantity': 20 
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+    
+    });
+};
---
+++ test/one_primary_ food_and_non_food_few_stc_few_sku/happy_path/item_movement/sorting-3.js
@@ -0,0 +1,42 @@
+const chai = require('chai');
+
+module.exports = function (token, request) {
+
+    describe('GET /customer-primaries/:id/shipments', function () {
+
+
+        it('it should sort ZEU300000001 successfully', function (done) {
+
+            request
+                .post('/item/sort')
+                .set('x-access-token', token)
+                .send({
+                    "source_container": "ZEU300000001",
+                    "destination_container": "GRC-001",
+                    "quantity": 10
+                })
+                .expect(200, function (err, result) {
+                    
+                    done();
+                });
+
+        });
+
+        it('it should sort ZEU300000001 successfully', function (done) {
+
+            request
+                .post('/item/sort')
+                .set('x-access-token', token)
+                .send({
+                    "source_container": "ZEU300000001",
+                    "destination_container": "GRC-008",
+                    "quantity": 10
+                })
+                .expect(200, function (err, result) {
+                    
+                    done();
+                });
+
+        });
+    });
+};
