0a261637a94893fda6a9d6287fb231cbced828a9
tanjarce <tanjarce@yahoo.com.ph>
2018-08-09 18:05:11 +0800

    add test for happy path test file 3

--- test/test-file-3/happy_path/item_movement/inbound-05.js
+++ test/test-file-3/happy_path/item_movement/inbound-05.js
@@ -11,11 +11,49 @@ module.exports = function (token, request) {
                 .set('x-access-token', token)
                 .type('json')
                 .send({
-                    'source_container_location': 'ID-180808-001',
+                    'source_container_location': 'ID-180809-002',
                     'source_container': 'MAX999',
                     'destination_container': 'ZEU500000001',
                     'material_id': '688368b7-adf3-4712-ae8d-2bca4e05eea8',
                     'quantity': 20
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+
+        it('it should receive ZEU550000001 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180809-003',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU550000001',
+                    'material_id': 'cc1c4bcd-cabb-4551-97c1-330100e0a625',
+                    'quantity': 48
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+
+        it('it should receive ZEU555000001 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180809-001',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU555000001',
+                    'material_id': '5668377a-0355-421e-a16d-3dbff3de16a4',
+                    'quantity': 58
                 })
                 .expect(200, function (err, result) {
                     chai.expect(result.body.data).to.have.property('items');
