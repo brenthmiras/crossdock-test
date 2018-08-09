a369fe791cf947d3e47690a24f810a205cac47d9
Ramil Barrera <ramilbarrera02.rb@gmail.com>
2018-08-09 14:03:06 +0800

    add test for happy path test-file-2

--- test/test-file-2/happy_path/item_movement/inbound.js
+++ test/test-file-2/happy_path/item_movement/inbound.js
@@ -15,7 +15,7 @@ module.exports = function (token, request) {
                     'source_container': 'MAX999',
                     'destination_container': 'ZEU010000001',
                     'material_id': '6bd1b793-3aa0-4f58-b7ba-e401d5a545fb',
-                    'quantity': 90
+                    'quantity': 105
                 })
                 .expect(200, function (err, result) {
                     done();
