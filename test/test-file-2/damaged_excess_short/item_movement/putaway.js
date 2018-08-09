55d222c26da2d0eafccebc54d7aa1ed0ad55574f
YongJo <eai.dalman@gmail.com>
2018-08-09 12:06:35 +0800

    add test for for damaged excess and short test file 2

---
+++ test/test-file-2/damaged_excess_short/item_movement/putaway.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+
+module.exports = function (token, request) {
+
+    describe('POST /item/putaway', function () {
+        it('it should putaway ZEU600000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU600000001',
+                    'destination_container_location': 'GSTG-B01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+    });
+};
