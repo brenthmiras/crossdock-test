fae74b3867a5e09e5311ed48b7862c0e17525723
codemagnus <brenthmiras2@gmail.com>
2018-08-09 16:35:28 +0800

    Add test for inbound and putaway for test file 3

--- test/test-file-3/happy_path/item_movement/inbound.js
+++ test/test-file-3/happy_path/item_movement/inbound.js
@@ -3,19 +3,19 @@ const chai = require('chai');
 module.exports = function (token, request) {
 
     describe('GET /inbound', function () {
-
-        it('it should receive ZEU000000001 successfully', function (done) {
+        // First test file
+        it('it should receive ZEU900000001 successfully', function (done) {
             // Pass
             request
                 .post('/item/receive')
                 .set('x-access-token', token)
                 .type('json')
                 .send({
-                    'source_container_location': 'ID-180808-001',
+                    'source_container_location': 'ID-180809-002',
                     'source_container': 'MAX999',
-                    'destination_container': 'ZEU000000001',
-                    'material_id': '27877c42-cf06-4407-96e1-3d3aa4a44ef2',
-                    'quantity': 18
+                    'destination_container': 'ZEU900000001',
+                    'material_id': 'c0b3ea8b-859e-40f5-8640-3bd6cfbc8f09',
+                    'quantity': 40
                 })
                 .expect(200, function (err, result) {
                     chai.expect(result.body.data).to.have.property('items');
@@ -23,45 +23,100 @@ module.exports = function (token, request) {
                 });
         });
 
-        it('it should receive ZEU000000002 successfully', function (done) {
+        it('it should receive ZEU900000002 successfully', function (done) {
             // Pass
             request
                 .post('/item/receive')
                 .set('x-access-token', token)
                 .type('json')
                 .send({
-                    'source_container_location': 'ID-180808-001',
+                    'source_container_location': 'ID-180809-002',
                     'source_container': 'MAX999',
-                    'destination_container': 'ZEU000000002',
-                    'material_id': '4b1c51e8-6420-4e3e-a361-444e44114fbd',
-                    'quantity': 1
+                    'destination_container': 'ZEU900000002',
+                    'material_id': 'c0b3ea8b-859e-40f5-8640-3bd6cfbc8f09',
+                    'quantity': 40
                 })
                 .expect(200, function (err, result) {
                     chai.expect(result.body.data).to.have.property('items');
                     done();
                 });
         });
-    
 
-        it('it should receive ZEU000000003 successfully', function (done) {
+        // Second test file
+        it('it should receive ZEU990000001 successfully', function (done) {
             // Pass
             request
                 .post('/item/receive')
                 .set('x-access-token', token)
                 .type('json')
                 .send({
-                    'source_container_location': 'ID-180808-001',
+                    'source_container_location': 'ID-180809-003',
                     'source_container': 'MAX999',
-                    'destination_container': 'ZEU000000003',
-                    'material_id': '90c8c84b-f5dc-46b3-8db7-ccfd1ba5bbc6',
-                    'quantity': 14
+                    'destination_container': 'ZEU990000001',
+                    'material_id': 'd20e1b31-02c5-498e-8a70-402c34a75a2d',
+                    'quantity': 60
                 })
                 .expect(200, function (err, result) {
                     chai.expect(result.body.data).to.have.property('items');
                     done();
                 });
         });
-    
-    
+
+        it('it should receive ZEU990000002 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180809-003',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU990000002',
+                    'material_id': 'd20e1b31-02c5-498e-8a70-402c34a75a2d',
+                    'quantity': 60
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+        it('it should receive ZEU990000003 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180809-003',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU990000003',
+                    'material_id': 'd20e1b31-02c5-498e-8a70-402c34a75a2d',
+                    'quantity': 20
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+
+        // Test file 3
+        it('it should receive ZEU999000001 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180809-001',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU999000001',
+                    'material_id': 'df4d5b38-d9b4-474b-bc0f-97c3cbe94817',
+                    'quantity': 15
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
     });
 };
--- test/test-file-3/happy_path/item_movement/putaway.js
+++ test/test-file-3/happy_path/item_movement/putaway.js
@@ -3,6 +3,7 @@ const chai = require('chai');
 module.exports = function (token, request) {
 
     describe('POST /item/putaway', function () {
+<<<<<<< Updated upstream
         it('it should putaway ZEU000000000 successfully', function (done) {
             request
                 .post('/item/putaway')
@@ -34,10 +35,16 @@ module.exports = function (token, request) {
 
 
         it('it should putaway ZEU000000002 successfully', function (done) {
+=======
+
+        // First test file
+        it('it should putaway ZEU900000001 successfully', function (done) {
+>>>>>>> Stashed changes
             request
                 .post('/item/putaway')
                 .set('x-access-token', token)
                 .send({
+<<<<<<< Updated upstream
                     'source_container': 'ZEU000000002',
                     'destination_container_location': 'GSTG-A01'
                 })
@@ -84,26 +91,39 @@ module.exports = function (token, request) {
                 .set('x-access-token', token)
                 .send({
                     'source_container': 'ZEU000000005',
+=======
+                    'source_container': 'ZEU900000001',
+>>>>>>> Stashed changes
                     'destination_container_location': 'GSTG-B01'
                 })
                 .expect(200, function (err, result) {
                     done();
                 });
+<<<<<<< Updated upstream
 
         });
 
 
         it('it should putaway ZEU000000006 successfully', function (done) {
+=======
+        });
+        it('it should putaway ZEU900000002 successfully', function (done) {
+>>>>>>> Stashed changes
             request
                 .post('/item/putaway')
                 .set('x-access-token', token)
                 .send({
+<<<<<<< Updated upstream
                     'source_container': 'ZEU000000006',
+=======
+                    'source_container': 'ZEU900000002',
+>>>>>>> Stashed changes
                     'destination_container_location': 'GSTG-B01'
                 })
                 .expect(200, function (err, result) {
                     done();
                 });
+<<<<<<< Updated upstream
 
         });
 
@@ -154,61 +174,101 @@ module.exports = function (token, request) {
 
 
         it('it should putaway ZEU000000010 successfully', function (done) {
+=======
+        });
+
+        // Second test file
+        it('it should putaway ZEU990000001 successfully', function (done) {
+>>>>>>> Stashed changes
             request
                 .post('/item/putaway')
                 .set('x-access-token', token)
                 .send({
+<<<<<<< Updated upstream
                     'source_container': 'ZEU000000010',
+=======
+                    'source_container': 'ZEU990000001',
+>>>>>>> Stashed changes
                     'destination_container_location': 'GSTG-A01'
                 })
                 .expect(200, function (err, result) {
                     done();
                 });
+<<<<<<< Updated upstream
 
         });
 
 
         it('it should putaway ZEU000000011 successfully', function (done) {
+=======
+        });
+        it('it should putaway ZEU990000002 successfully', function (done) {
+>>>>>>> Stashed changes
             request
                 .post('/item/putaway')
                 .set('x-access-token', token)
                 .send({
+<<<<<<< Updated upstream
                     'source_container': 'ZEU000000011',
+=======
+                    'source_container': 'ZEU990000002',
+>>>>>>> Stashed changes
                     'destination_container_location': 'GSTG-A01'
                 })
                 .expect(200, function (err, result) {
                     done();
                 });
+<<<<<<< Updated upstream
 
         });
 
 
         it('it should putaway ZEU000000012 successfully', function (done) {
+=======
+        });
+        it('it should putaway ZEU990000003 successfully', function (done) {
+>>>>>>> Stashed changes
             request
                 .post('/item/putaway')
                 .set('x-access-token', token)
                 .send({
+<<<<<<< Updated upstream
                     'source_container': 'ZEU000000012',
+=======
+                    'source_container': 'ZEU990000003',
+>>>>>>> Stashed changes
                     'destination_container_location': 'GSTG-A01'
                 })
                 .expect(200, function (err, result) {
                     done();
                 });
+<<<<<<< Updated upstream
 
         });
 
 
         it('it should putaway ZEU000000013 successfully', function (done) {
+=======
+        });
+
+        // Third test file
+        it('it should putaway ZEU999000001 successfully', function (done) {
+>>>>>>> Stashed changes
             request
                 .post('/item/putaway')
                 .set('x-access-token', token)
                 .send({
+<<<<<<< Updated upstream
                     'source_container': 'ZEU000000013',
+=======
+                    'source_container': 'ZEU999000001',
+>>>>>>> Stashed changes
                     'destination_container_location': 'GSTG-A01'
                 })
                 .expect(200, function (err, result) {
                     done();
                 });
+<<<<<<< Updated upstream
 
         });
 
@@ -229,5 +289,8 @@ module.exports = function (token, request) {
 
 
 
+=======
+        });
+>>>>>>> Stashed changes
     });
 };
