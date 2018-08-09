b00017c009e0215d6db23a8ad992f3048202058b
userJerald <petter.parker.user.jerald@gmail.com>
2018-08-09 13:40:55 +0800

    initial setup for test-file-3

---
+++ test/test-file-3/damaged_excess_short/item_movement/inbound-04.js
@@ -0,0 +1,65 @@
+const chai = require('chai');
+
+module.exports = function (token, request) {
+
+    describe('GET /inbound', function () {
+
+        it('it should receive ZEU400000001 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180808-001',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU400000001',
+                    'material_id': '6403f377-de2b-44dd-b374-1c23157e5cfe',
+                    'quantity': 40
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+
+        it('it should receive ZEU400000002 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180808-001',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU400000002',
+                    'material_id': '6403f377-de2b-44dd-b374-1c23157e5cfe',
+                    'quantity': 40
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+
+
+        it('it should receive ZEU400000003 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180808-001',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU400000003',
+                    'material_id': '6403f377-de2b-44dd-b374-1c23157e5cfe',
+                    'quantity': 25
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+    });
+};
---
+++ test/test-file-3/happy_path/item_movement/putaway-04.js
@@ -0,0 +1,50 @@
+const chai = require('chai');
+
+module.exports = function (token, request) {
+
+    describe('POST /item/putaway', function () {
+        it('it should putaway ZEU400000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU400000001',
+                    'destination_container_location': 'GSTG-A01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+
+
+        it('it should putaway ZEU400000002 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU400000002',
+                    'destination_container_location': 'GSTG-A01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+
+
+        it('it should putaway ZEU400000003 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU400000003',
+                    'destination_container_location': 'GSTG-A01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+    });
+};
