8e91a83f1b788173182df36a48df9f19ddd06d81
Jem <jetorres@codedisruptors.com>
2018-08-09 12:05:46 +0800

    for damaged excess and short

--- test/test-file-2/happy_path/item_movement/sorting.js
+++ test/test-file-2/happy_path/item_movement/sorting.js
@@ -3,19 +3,6 @@ const chai = require('chai');
 module.exports = function (token, request) {
 
     describe('POST /customer-primaries/:id/shipments', function () {
-        it('it should sort ZEU010000001 successfully', function (done) {
-            request
-                .post('/item/sort')
-                .set('x-access-token', token)
-                .send({
-                    "source_container": "ZEU010000001",
-                    "destination_container": "GRC-015",
-                    "quantity": 20
-                })
-                .expect(200, function (err, result) {
-                    done();
-                });
-        });
 
         it('it should sort ZEU010000001 successfully', function (done) {
             request
@@ -24,13 +11,28 @@ module.exports = function (token, request) {
                 .send({
                     "source_container": "ZEU010000001",
                     "destination_container": "GRC-011",
-                    "quantity": 80
+                    "quantity": 85
                 })
                 .expect(200, function (err, result) {
                     done();
                 });
         });
 
+        it('it should sort ZEU010000001 successfully', function (done) {
+            request
+                .post('/item/sort')
+                .set('x-access-token', token)
+                .send({
+                    "source_container": "ZEU010000001",
+                    "destination_container": "GRC-015",
+                    "quantity": 5
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+       
         it('it should sort ZEU990000001 successfully', function (done) {
             request
                 .post('/item/sort')
@@ -59,21 +61,22 @@ module.exports = function (token, request) {
                 });
         });
 
-         it('it should sort ZEU990000002 successfully', function (done) {
+
+        it('it should sort ZEU990000002 successfully', function (done) {
             request
                 .post('/item/sort')
                 .set('x-access-token', token)
                 .send({
-                    "source_container": "ZEU990000001",
-                    "destination_container": "ZEU-C01-02",
-                    "quantity": 20
+                    "source_container": "ZEU990000002",
+                    "destination_container": "GRC-011",
+                    "quantity": 40
                 })
                 .expect(200, function (err, result) {
                     done();
                 });
         });
 
-        it('it should sort ZEU990000002 successfully', function (done) {
+         it('it should sort ZEU990000002 successfully', function (done) {
             request
                 .post('/item/sort')
                 .set('x-access-token', token)
@@ -87,13 +90,13 @@ module.exports = function (token, request) {
                 });
         });
 
-          it('it should sort ZEU990000003 successfully', function (done) {
+        it('it should sort ZEU990000003 successfully', function (done) {
             request
                 .post('/item/sort')
                 .set('x-access-token', token)
                 .send({
                     "source_container": "ZEU990000003",
-                    "destination_container": "GRC-015",
+                    "destination_container": "ZEU-C01-02",
                     "quantity": 20
                 })
                 .expect(200, function (err, result) {
