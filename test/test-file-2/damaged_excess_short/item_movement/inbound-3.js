66412769b275efb33ea86191c79bb1a448c5d2e7
stacieH <stacie.harlene@gmail.com>
2018-08-08 23:06:56 -0500

    add test for damaged excess and short test file 2

---
+++ test/test-file-2/damaged_excess_short/item_movement/inbound-3.js
@@ -0,0 +1,46 @@
+const chai = require('chai');
+
+module.exports = function (token, request) {
+
+    describe('GET /inbound', function () {
+
+        it('it should receive ZEU330000001 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180809-001',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU330000001',
+                    'material_id': '90c8c84b-f5dc-46b3-8db7-ccfd1ba5bbc6',
+                    'quantity': 14 
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+
+        
+        it('it should receive 9 excess ZEU330000002', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180809-001-EXCESS',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU330000002',
+                    'material_id': '90c8c84b-f5dc-46b3-8db7-ccfd1ba5bbc6',
+                    'quantity': 9 
+                })
+                .expect(200, function (err, result) {
+                    chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+    });
+};
---
+++ test/test-file-2/damaged_excess_short/item_movement/putaway-3.js
@@ -0,0 +1,35 @@
+const chai = require('chai');
+
+module.exports = function (token, request) {
+
+    describe('POST /item/putaway', function () {
+        it('it should putaway ZEU330000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU330000001',
+                    'destination_container_location': 'GSTG-A01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+
+        
+        it('it should putaway 9 excess ZEU330000002', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU330000002',
+                    'destination_container_location': 'EXCESS'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+    });
+};
