50a6eed8058094c817a59db1b5f68b8fcc81643b
Jem <jetorres@codedisruptors.com>
2018-08-10 15:31:19 +0800

    add test for damaged excess short path test file 3

---
+++ test/test-file-3/damaged_excess_short/item_movement/inbound.js
@@ -0,0 +1,123 @@
+const chai = require('chai');
+
+module.exports = function (token, request) {
+
+// JF000898
+// 616702
+// 
+    describe('POST /inbound', function () {
+    
+        //Inbound doc 1 
+        it('it should receive ZEU000000001 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180810-001',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU000000001',
+                    'material_id': 'faeaab5a-46c8-41a6-8c2f-861741426e47',
+                    'quantity': 2
+                })
+                .expect(200, function (err, result) {
+                    // chai.expect(result.body.data).to.have.property('items');
+                    done();
+                });
+        });
+
+        //Inbound doc 2
+        it('it should receive ZEU010000001 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180810-002',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU010000001',
+                    'material_id': 'e9dd8159-7ed5-4778-9723-7e98aa8fce9e',
+                    'quantity': 40
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should receive ZEU010000002 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180810-002',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU010000002',
+                    'material_id': 'e9dd8159-7ed5-4778-9723-7e98aa8fce9e',
+                    'quantity': 40
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should receive ZEU010000003 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180810-002',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU010000003',
+                    'material_id': 'e9dd8159-7ed5-4778-9723-7e98aa8fce9e',
+                    'quantity': 40
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should receive ZEU010000004 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180810-002',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU010000004',
+                    'material_id': 'e9dd8159-7ed5-4778-9723-7e98aa8fce9e',
+                    'quantity': 39
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+        
+        //Inbound doc 3
+        it('it should receive ZEU011000001 successfully', function (done) {
+            // Pass
+            request
+                .post('/item/receive')
+                .set('x-access-token', token)
+                .type('json')
+                .send({
+                    'source_container_location': 'ID-180810-003',
+                    'source_container': 'MAX999',
+                    'destination_container': 'ZEU011000001',
+                    'material_id': '6bd1b793-3aa0-4f58-b7ba-e401d5a545fb',
+                    'quantity': 104
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+        
+    });
+};
---
+++ test/test-file-3/damaged_excess_short/item_movement/sorting.js
@@ -0,0 +1,110 @@
+const chai = require('chai');
+
+module.exports = function (token, request) {
+
+    describe('POST /customer-primaries/:id/shipments', function () {
+
+        // Inbound doc 1
+        it('it should sort ZEU000000001 successfully', function (done) {
+            request
+                .post('/item/sort')
+                .set('x-access-token', token)
+                .send({
+                    "source_container": "ZEU000000001",
+                    "destination_container": "GRC-028",
+                    "quantity": 2
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        // Inbound doc 2
+
+         it('it should sort ZEU010000001 successfully', function (done) {
+            request
+                .post('/item/sort')
+                .set('x-access-token', token)
+                .send({
+                    "source_container": "ZEU010000001",
+                    "destination_container": "ZEU-A04-02",
+                    "quantity": 40
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should sort ZEU010000002 successfully', function (done) {
+            request
+                .post('/item/sort')
+                .set('x-access-token', token)
+                .send({
+                    "source_container": "ZEU010000002",
+                    "destination_container": "ZEU-A05-01",
+                    "quantity": 40
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+         it('it should sort ZEU010000003 successfully', function (done) {
+            request
+                .post('/item/sort')
+                .set('x-access-token', token)
+                .send({
+                    "source_container": "ZEU010000003",
+                    "destination_container": "ZEU-A03-01",
+                    "quantity": 40
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+          it('it should sort ZEU010000004 successfully', function (done) {
+            request
+                .post('/item/sort')
+                .set('x-access-token', token)
+                .send({
+                    "source_container": "ZEU010000004",
+                    "destination_container": "GRC-039",
+                    "quantity": 39
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        // Inbound doc 3
+
+         it('it should sort ZEU011000001 successfully', function (done) {
+            request
+                .post('/item/sort')
+                .set('x-access-token', token)
+                .send({
+                    "source_container": "ZEU011000001",
+                    "destination_container": "GRC-019",
+                    "quantity": 19
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+         it('it should sort ZEU011000001 successfully', function (done) {
+            request
+                .post('/item/sort')
+                .set('x-access-token', token)
+                .send({
+                    "source_container": "ZEU011000001",
+                    "destination_container": "GRC-014",
+                    "quantity": 85
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+    });
+};
