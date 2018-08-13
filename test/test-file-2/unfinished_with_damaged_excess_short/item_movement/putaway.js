9d887f4197a4cb9719b98da9767213a2eb441020
diane <diane@codedisruptors.com>
2018-08-13 14:07:37 +0800

    added test script for unfinished with damaged excess short

---
+++ test/test-file-2/unfinished_with_damaged_excess_short/item_movement/putaway.js
@@ -0,0 +1,363 @@
+module.exports = function (token, request) {
+
+    describe('GET /item/putaway', function () {
+
+        it('it should putaway ZEU010000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU010000001',
+                    'destination_container_location': 'GSTG-B01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should putaway ZEU010000002 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU010000002',
+                    'destination_container_location': 'GSTG-B01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should putaway ZEU010000003 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU010000003',
+                    'destination_container_location': 'GSTG-B01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should putaway ZEU010000004 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU010000004',
+                    'destination_container_location': 'DAMAGED'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+        
+        it('it should putaway ZEU100000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU110000001',
+                    'destination_container_location': 'GSTG-A01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+
+        it('it should putaway excess ZEU110000002 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU110000002',
+                    'destination_container_location': 'EXCESS'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+
+        it('it should putaway ZEU2200000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU220000001',
+                    'destination_container_location': 'GSTG-B01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should putaway 3 excess ZEU2200000002 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU220000002',
+                    'destination_container_location': 'EXCESS'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        }); 
+        
+        it('it should putaway ZEU330000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU330000001',
+                    'destination_container_location': 'GSTG-A01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+
+
+        it('it should putaway 9 excess ZEU330000002', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU330000002',
+                    'destination_container_location': 'EXCESS'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+        it('it should putaway ZEU440000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU440000001',
+                    'destination_container_location': 'GSTG-A01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+
+        it('it should putaway ZEU440000002 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU440000002',
+                    'destination_container_location': 'GSTG-A01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+
+
+        it('it should putaway 2 Damaged 20046527 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU440000003',
+                    'destination_container_location': 'DAMAGED'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should putaway ZEU550000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU550000001',
+                    'destination_container_location': 'GSTG-A01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+        it('it should putaway 10 EXCESS 20062058', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU550000002',
+                    'destination_container_location': 'EXCESS'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+
+        it('it should putaway ZEU660000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU660000001',
+                    'destination_container_location': 'GSTG-B01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+        it('it should putaway 7 excess 21066581', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU660000002',
+                    'destination_container_location': 'EXCESS'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should putaway ZEU770000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU770000001',
+                    'destination_container_location': 'GSTG-A01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+        it('it should putaway 3 excess 20046526', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU770000002',
+                    'destination_container_location': 'EXCESS'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should putaway ZEU880000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU880000001',
+                    'destination_container_location': 'GSTG-B01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+        it('it should putaway ZEU880000002 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU880000002',
+                    'destination_container_location': 'GSTG-B01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+        it('it should putaway ZEU880000003 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU880000003',
+                    'destination_container_location': 'DAMAGED'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+
+        });
+
+        it('it should putaway ZEU990000001 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU990000001',
+                    'destination_container_location': 'GSTG-B01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+
+        it('it should putaway ZEU990000002 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU990000002',
+                    'destination_container_location': 'GSTG-B01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        //uncomment to move data to yesterday and continue receiving today
+        it('it should simulate date -1', function (done) {
+            request
+                .post('/test/minus_one')
+                .set('x-access-token', token)
+                .send({
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should putaway ZEU990000003 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU990000003',
+                    'destination_container_location': 'GSTG-B01'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+
+        it('it should putaway ZEU990000004 successfully', function (done) {
+            request
+                .post('/item/putaway')
+                .set('x-access-token', token)
+                .send({
+                    'source_container': 'ZEU990000004',
+                    'destination_container_location': 'DAMAGED'
+                })
+                .expect(200, function (err, result) {
+                    done();
+                });
+        });
+    });
+};
