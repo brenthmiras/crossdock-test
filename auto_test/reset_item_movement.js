ace11fdfc6f54a35fc0da67e45a1823560af7661
diane <diane@codedisruptors.com>
2018-08-16 09:21:11 +0800

    added reset scrpts

---
+++ auto_test/reset_item_movement.js
@@ -0,0 +1,48 @@
+const config = require('../config');
+const async = require('async');
+
+const request = require('supertest')(config.BASE_URL);
+const chai = require('chai');
+
+let token;
+
+describe('Login user to get token', function () {
+    it('should be successful', function (done) {
+        this.timeout(20000);
+        request
+            .post('/auth/login')
+            .type('json')
+            .set('Content-Type', 'application/json')
+            .send({
+                email: 'admin@cdi.com',
+                password: 'secret'
+            })
+
+            .end(function (err, result) {
+                token = result.header['x-access-token'];
+                done();
+            });
+    });
+
+});
+
+describe('RESET ITEM MOVEMENTS', function () {
+
+    it('should reset item movements', function(done){
+        request
+        .get('/reset-item-movements')
+        .type('json')
+        .set('x-access-token', token)
+        .expect(200, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            done();
+        });
+
+    });
+});
+
+
+
+
