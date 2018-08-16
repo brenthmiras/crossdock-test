b1ce967e5cb00696ab870f8f8833cf19113a17c2
Ramil Barrera <ramilbarrera02.rb@gmail.com>
2018-08-16 10:47:07 +0800

    add minus and plus 1

---
+++ auto_test/minus1.js
@@ -0,0 +1,36 @@
+const config = require('../config');
+const request = require('supertest')(config.BASE_URL);
+
+let token;
+
+describe('Login user to get token', function () {
+    it('should be successful', function (done) {
+        this.timeout(20000);
+        request
+            .post('/auth/login')
+            .type('json')
+            .set('Content-Type', 'application/json')
+            .send({
+                email: 'admin@cdi.com',
+                password: 'secret'
+            })
+            .end(function (err, result) {
+                token = result.header['x-access-token'];
+                console.log(token);
+                done();
+        });
+    });
+});
+
+describe('POST minus 1', function () {
+    it('should be succesful', function (done) {
+        request
+            .post('/test/minus_one')
+            .type('json')
+            .set('x-access-token', token)
+            .expect(200, function (err, result) {
+                if(err) throw err;
+                done();
+        });
+    });
+});
