195b9ee84a81fc40ef53f3c6c2369052feeda44a
diane <diane@codedisruptors.com>
2018-08-22 10:03:37 +0800

    added actual files used by ops

--- config.js
+++ config.js
@@ -16,6 +16,30 @@ module.exports = {
         {
             title: 'ULP - With two invoice per sku, same customer',
             path: '../prealerts/ULP-two-invoice-same-STC.csv'
+        },
+        {
+            title: 'SCJ PRE ALERT JUNE 20_v2 (actual file)',
+            path: '../prealerts/SCJ_PRE_ALERT_JUNE_20_v2.csv'
+        },
+        {
+            title: 'ZEU-ULP-180727-003 (actual file)',
+            path: '../prealerts/ZEU-ULP-180727-003.csv'
+        },
+        {
+            title: 'ZEU-ULP-180727-004(actual file)',
+            path: '../prealerts/ZEU-ULP-180727-004.csv'
+        },
+        {
+            title: 'ZEU-ULP-180727-005 (actual file)',
+            path: '../prealerts/ZEU-ULP-180727-005.csv'
+        },
+        {
+            title: 'RFM-SM (actual file)',
+            path: '../prealerts/RFM-SM.csv'
+        },
+        {
+            title: 'RFM-NON-SM (actual file)',
+            path: '../prealerts/RFM-NON-SM.csv'
         }
     ]
 };
