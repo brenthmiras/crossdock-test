bc32f3f2f05ba1d16fcf47f7b6a8eb11178fcad6
diane <diane@codedisruptors.com>
2018-08-22 10:28:50 +0800

    added actual files from aug 6

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    BASE_URL: 'http://test.codedisruptors.com:8001',
+    BASE_URL: 'http://test.codedisruptors.com:10001',
     FILES: [
         {
             title: 'With 1 primary, few secondary, few sku (non-food only)',
@@ -16,10 +16,6 @@ module.exports = {
         {
             title: 'ULP - With two invoice per sku, same customer',
             path: '../prealerts/ULP-two-invoice-same-STC.csv'
-        },
-        {
-            title: 'SCJ PRE ALERT JUNE 20_v2 (actual file)',
-            path: '../prealerts/SCJ_PRE_ALERT_JUNE_20_v2.csv'
         },
         {
             title: 'ZEU-ULP-180727-003 (actual file)',
@@ -40,6 +36,31 @@ module.exports = {
         {
             title: 'RFM-NON-SM (actual file)',
             path: '../prealerts/RFM-NON-SM.csv'
+        },
+        {
+            title: 'ZEU-RFM-180806-005 (RFM actual file Aug 6)',
+            path: '../prealerts/ZEU-RFM-180806-005.csv'
+        },
+        {
+            title: 'ZEU-RFM-180806-004 (RFM actual file Aug 6)',
+            path: '../prealerts/ZEU-RFM-180806-004.csv'
+        },
+        {
+            title: 'ZEU-ULP-180806-001 (ULP actual file Aug 6)',
+            path: '../prealerts/ZEU-ULP-180806-001.csv'
+        },
+        {
+            title: 'ZEU-SCJ-180806-002 (SCJ actual file Aug 6)',
+            path: '../prealerts/ZEU-SCJ-180806-002.csv'
+        },
+        {
+            title: 'ZEU-GSM-180806-003 (GSM actual file Aug 6)',
+            path: '../prealerts/ZEU-GSM-180806-003.csv'
         }
     ]
 };
+//ZEU-RFM-180806-005
+//ZEU-RFM-180806-004
+//ZEU-GSM-180806-003
+//ZEU-ULP-180806-001
+//ZEU-SCJ-180806-002
